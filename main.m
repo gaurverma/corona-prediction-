86c5052bf93e1d7813adb3dda6ab2ba17e7cd0c1
gaurverma <55631123+gaurverma@users.noreply.github.com>
2020-04-02 14:01:13 +0530

    Add files via upload

---
+++ main.m
@@ -0,0 +1,44 @@
+% initialize
+clear; close all,clc;
+% load the data 
+data =load('corona.txt');
+X = data(:, 1:5);
+y = data(:,6);
+
+[m, n] = size(X);
+
+% Add intercept term to x and X_test
+X = [ones(m, 1) X];
+
+% Initialize fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Gradient at initial theta (zeros): \n');
+fprintf(' %f \n', grad);
+
+%% ============= Part 3: Optimizing using fminunc  =============
+%  In this part, we will use a built-in function (fminunc) to find the
+%  optimal parameters theta.
+
+%  Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost 
+[theta, cost] = ...
+	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Print theta to screen
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+fprintf('theta: \n');
+fprintf(' %f \n', theta);
+
+%% ============== Part 4: Predict and Accuracies ==============
+% Compute accuracy on our training set
+p = predict(theta, X);
+
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
